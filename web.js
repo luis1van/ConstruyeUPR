fc2570d5cd88acb1a948cc2f007275b7be9cf7a1
Luis Padro <lpadro@construye.upr.edu>
2016-11-19 19:07:49 -0400

    l

--- web.js
+++ web.js
@@ -3,13 +3,13 @@ var serveStatic = require('serve-static')
  
 var app = express()
 
-port = parseInt(process.env.PORT, 10) || 80,
+port = parseInt(process.env.PORT, 10) || 8080,
 publicDir = process.argv[2] || __dirname + '/public';
 
 
 
 
-app.use('/img', express.static(path.join(publicDir, 'img')))
+app.use('/img', express.static(publicDir+'/img'))
 
 app.use(serveStatic(publicDir, {'index': ['index.html', 'index.htm']}))
 app.listen(port)
