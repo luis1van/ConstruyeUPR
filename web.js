42c00b517567664dcbc79b8c21b6d25dd22251f3
luis1van <luis.padro2@upr.edu>
2016-11-19 18:09:47 -0500

    vamo a ver

--- web.js
+++ web.js
@@ -5,6 +5,7 @@ var app = express()
 
 port = parseInt(process.env.PORT, 10) || 3000,
 publicDir = process.argv[2] || __dirname + '/public';
- 
+
+app.use('/img', express.static(path.join(publicDir, 'img')))
 app.use(serveStatic(publicDir, {'index': ['index.html', 'index.htm']}))
 app.listen(port)
