ae7c4e9be131de8afc2069360faf6d66fd38b3fb
luis1van <luis.padro2@upr.edu>
2016-11-19 18:14:31 -0500

    Revert "vamo a ver"
    This reverts commit 42c00b517567664dcbc79b8c21b6d25dd22251f3.

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 
 $.material.init();
 var slider = new Slider("#ex13", {
+    ticks: [0, 100, 200, 300, 400],
+    ticks_labels: ['$0', '$100', '$200', '$300', '$400'],
     tooltip: 'hide'
 });
 
--- public/recinto/Ponce/js/ponce.js
+++ public/recinto/Ponce/js/ponce.js
@@ -1,3 +1,2 @@
-
 var style = $('<style>.slider-tick.round.in-selection{\n     background-image:linear-gradient(to bottom, #ef3e35 0%, #ef3e35 100%) !important; \n}\n.slider-selection.tick-slider-selection{\n     background-image:linear-gradient(to bottom, #ef3e35 0%, #ef3e35 100%) !important; \n} </style>');
 $('html > head').append(style);
